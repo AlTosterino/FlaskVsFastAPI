f7905bfd2248e0c057ce72584cc7d98d4926c1c5
AlTosterino <altosterino@gmail.com>
2021-02-23 14:07:36 +0100

    Add post and get operations to FastApi with minimal db repository

--- fastapi/src/fastapi_app/web_app/app.py
+++ fastapi/src/fastapi_app/web_app/app.py
@@ -1,8 +1,55 @@
-from fastapi import FastAPI
+from dataclasses import dataclass, field
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Creator(BaseModel):
+    first_name: str
+    last_name: str
 
-@app.get("/")
-def get_root():
-    return {"Hello": "World"}
+class NewsInput(BaseModel):
+    title: str
+    content: str
+    creator: Creator
+
+class News(NewsInput):
+    id: int
+
+class DatabaseRepositoryError(Exception):
+    pass
+
+@dataclass
+class NewsRepository:
+    _last_id: int = field(init=False, default=0)
+    _database: List[News] = field(init=False, default_factory=list)
+
+    def save(self, news_input: NewsInput) -> News:
+        self._last_id += 1
+        news_to_save = News(**{"id": self._last_id, **news_input.dict()})
+        self._database.append(news_to_save)
+        return news_to_save
+
+    def get(self, news_id: int) -> News:
+        for news in self._database:
+            if news.id == news_id:
+                return news
+        raise DatabaseRepositoryError(f"News with id {news_id} don't exist")
+
+DATABASE_REPOSITORY = NewsRepository()
+
+
+
+@app.post("/news", response_model=News)
+def add_news(news_input: NewsInput):
+    return DATABASE_REPOSITORY.save(news_input=news_input)
+
+@app.get("/news/{news_id}", response_model=News)
+def get_news(news_id: int):
+    try:
+        db_news = DATABASE_REPOSITORY.get(news_id=news_id)
+    except DatabaseRepositoryError as err:
+        raise HTTPException(status_code=404, detail=str(err))
+    return db_news
